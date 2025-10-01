6223163a2888b0ddc2ec46dc2d89adc7f78f3fe6
Milena Barcia <143225847+milenabarcia@users.noreply.github.com>
2025-10-01 19:16:08 -0300

    Update

--- src/main/java/com/example/ac1/Ac1Application.java
+++ src/main/java/com/example/ac1/Ac1Application.java
@@ -29,24 +29,21 @@ public class Ac1Application implements CommandLineRunner {
     @Override
     public void run(String... args) {
 
-        // Criando diretores
+       
         diretor d1 = new diretor(null, "Christopher Nolan", null);
         diretor d2 = new diretor(null, "Quentin Tarantino", null);
 
-        // Criando filmes
+       
         filmes f1 = new filmes(null, "Inception", 148, d1);
         filmes f2 = new filmes(null, "Interstellar", 169, d1);
         filmes f3 = new filmes(null, "Pulp Fiction", 154, d2);
         filmes f4 = new filmes(null, "Django Unchained", 165, d2);
 
-        // Relacionando filmes aos diretores
         d1.setFilmes(Arrays.asList(f1, f2));
         d2.setFilmes(Arrays.asList(f3, f4));
 
-        // Salvando diretores (e os filmes em cascata)
         diretorRepository.saveAll(Arrays.asList(d1, d2));
 
-        // ---------------- Testando FilmeRepository ----------------
         System.out.println("\n=== Filmes com duração > 150 minutos ===");
         filmeRepository.findByDuracaoGreaterThan(150)
                 .forEach(f -> System.out.println(f.getTitulo() + " - " + f.getDuracao()));
@@ -59,7 +56,6 @@ public class Ac1Application implements CommandLineRunner {
         filmeRepository.findByTituloStartingWith("In")
                 .forEach(f -> System.out.println(f.getTitulo()));
 
-        // ---------------- Testando DiretorRepository ----------------
         System.out.println("\n=== Diretores cujo nome começa com 'C' ===");
         diretorRepository.findByNomeStartingWith("C")
                 .forEach(d -> System.out.println(d.getNome()));
